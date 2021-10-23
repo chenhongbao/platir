94061f40025242d610b75ca3bb5aca41dd1cd992
陈宏葆 <chenhongbao@outlook.com>
2021-10-23 11:17:37 +0800

    Move test sample classes into test class.

--- platir-core/src/test/java/io/platir/core/internal/CompositeStrategyTest.java
+++ platir-core/src/test/java/io/platir/core/internal/CompositeStrategyTest.java
@@ -2,7 +2,24 @@ package io.platir.core.internal;
 
 import io.platir.core.AnnotationParsingException;
 import io.platir.queries.FactoryImpl;
+import io.platir.service.Bar;
 import io.platir.service.Factory;
+import io.platir.service.PlatirClient;
+import io.platir.service.Strategy;
+import io.platir.service.Tick;
+import io.platir.service.Trade;
+import io.platir.service.TradeUpdate;
+import io.platir.service.annotation.OnBar;
+import io.platir.service.annotation.OnDestroy;
+import io.platir.service.annotation.OnStart;
+import io.platir.service.annotation.OnStop;
+import io.platir.service.annotation.OnTick;
+import io.platir.service.annotation.OnTrade;
+import io.platir.service.annotation.OnTradeUpdate;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -125,4 +142,141 @@ public class CompositeStrategyTest {
         assertEquals(2, strategy.tradeUpdateCounts().size(), "Notice callback not hit.");
     }
 
+    private static class AnnotationStrategy {
+
+        private final Map<Bar, List<String>> barCounts = new HashMap<>();
+        private final Map<Tick, List<String>> tickCounts = new HashMap<>();
+        private final Map<Trade, List<String>> tradeCounts = new HashMap<>();
+        private final List<String> destroyCounts = new LinkedList<>();
+        private final List<String> tradeUpdateCounts = new LinkedList<>();
+        private final List<String> startCounts = new LinkedList<>();
+        private final List<String> stopCounts = new LinkedList<>();
+
+        Map<Bar, List<String>> barCounts() {
+            return new HashMap<>(barCounts);
+        }
+
+        Map<Tick, List<String>> tickCounts() {
+            return new HashMap<>(tickCounts);
+        }
+
+        Map<Trade, List<String>> tradeCounts() {
+            return new HashMap<>(tradeCounts);
+        }
+
+        List<String> destroyCounts() {
+            return destroyCounts;
+        }
+
+        List<String> tradeUpdateCounts() {
+            return tradeUpdateCounts;
+        }
+
+        List<String> startCounts() {
+            return startCounts;
+        }
+
+        List<String> stopCounts() {
+            return stopCounts;
+        }
+
+        void addBarCount(Bar bar, String method) {
+            var list = barCounts.computeIfAbsent(bar, key -> new LinkedList<String>());
+            list.add(method);
+        }
+
+        @OnBar(id = {"c2201", "rb2210"})
+        public void whenBar(Bar bar) {
+            addBarCount(bar, "@OnBar(id = {\"c2201\", \"br2210\"})");
+        }
+
+        @OnBar(id = "rb2210", minute = {1, 5})
+        public void whenBarRB2210(Bar bar) {
+            addBarCount(bar, "OnBar(id = \"rb2210\", minute = {1, 5})");
+        }
+
+        @OnDestroy
+        public void whenDestroy() {
+            destroyCounts.add("@OnDestroy");
+        }
+
+        @OnTradeUpdate
+        public void whenTradeUpdate(TradeUpdate update) {
+            tradeUpdateCounts.add("@OnTradeUpdate");
+        }
+
+        @OnStart
+        public void whenStart(String[] args, PlatirClient client) {
+            startCounts.add("@OnStart");
+        }
+
+        @OnStop
+        public void whenStop(int reason) {
+            stopCounts.add("@OnStop");
+        }
+
+        void addTickCount(Tick tick, String method) {
+            var list = tickCounts.computeIfAbsent(tick, key -> new LinkedList<String>());
+            list.add(method);
+        }
+
+        @OnTick(id = {"c2205", "c2209"})
+        public void whenTickC(Tick tick) {
+            addTickCount(tick, "@OnTick(id = {\"c2205\", \"c2201\"})");
+        }
+
+        @OnTick(id = "rb2210")
+        public void whenTickRB2210(Tick tick) {
+            addTickCount(tick, "@OnTick(id = \"rb2210\")");
+        }
+
+        void addTradeCount(Trade trade, String method) {
+            var list = tradeCounts.computeIfAbsent(trade, key -> new LinkedList<String>());
+            list.add(method);
+        }
+
+        @OnTrade
+        public void whenTrade(Trade trade) {
+            addTradeCount(trade, "@OnTrade");
+        }
+    }
+
+    private static class SuperStrategy extends AnnotationStrategy implements Strategy {
+
+        @Override
+        public void onStart(String[] args, PlatirClient platir) {
+            startCounts().add("onStart()");
+        }
+
+        @Override
+        public void onStop(int reason) {
+            stopCounts().add("onStop()");
+        }
+
+        @Override
+        public void onDestroy() {
+            destroyCounts().add("onDestroy()");
+        }
+
+        @Override
+        public void onTrade(Trade trade) {
+            addTradeCount(trade, "onTrade()");
+        }
+
+        @Override
+        public void onTick(Tick tick) {
+            addTickCount(tick, "onTick()");
+        }
+
+        @Override
+        public void onBar(Bar bar) {
+            addBarCount(bar, "onBar()");
+        }
+
+        @Override
+        public void onTradeUpdate(TradeUpdate notice) {
+            tradeUpdateCounts().add("onNotice()");
+        }
+
+    }
 }
