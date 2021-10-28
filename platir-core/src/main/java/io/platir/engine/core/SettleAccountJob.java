80b5029eb01d73ac995b8c709dd3995423f51a37
陈宏葆 <chenhongbao@outlook.com>
2021-10-28 12:29:40 +0800

    Check account removed before settling it.

--- platir-core/src/main/java/io/platir/engine/core/SettleAccountJob.java
+++ platir-core/src/main/java/io/platir/engine/core/SettleAccountJob.java
@@ -7,28 +7,35 @@ import java.time.LocalDateTime;
 import java.util.logging.Level;
 
 public class SettleAccountJob implements TimerJob {
-    
+
     private final AccountCore account;
-    
+
     public SettleAccountJob(AccountCore account) {
         this.account = account;
     }
-    
+
     @Override
     public void onTime(LocalDateTime datetime, EngineTimer timer) {
-        try {
-            var setting = account.getAccountSetting();
-            if (setting.settlementTime().check(datetime)) {
+        if (isAccountRemoved()) {
+            timer.removeJob(this);
+            return;
+        }
+        var setting = account.getAccountSetting();
+        if (setting.settlementTime().check(datetime)) {
+            try {
                 settleAccount();
+            } catch (Throwable throwable) {
+                PlatirEngineCore.logger().log(Level.SEVERE, "Account({0}) settlement throws exception. {1}", new Object[]{account.getAccountId(), throwable.getMessage()});
             }
-            //TODO If account is removed, need to remove the job.
-        } catch (Throwable throwable) {
-            PlatirEngineCore.logger().log(Level.SEVERE, "Account({0}) settlement throws exception. {1}", new Object[]{account.getAccountId(), throwable.getMessage()});
         }
     }
-    
+
     private void settleAccount() {
         //TODO Implement settleAccount() in SettleAccountJob.
     }
-    
+
+    private boolean isAccountRemoved() {
+        return !account.getUser().accounts().containsValue(account);
+    }
+
 }
